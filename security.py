fd223f15fc46252da563e70684013f8734d10759
neilvillamizar <neilvillamizar@gmail.com>
2020-02-08 14:15:36 -0400

    comienzo pruebas regex, todas aprovadas hasta el momento

---
+++ security.py
@@ -0,0 +1,21 @@
+import re 
+
+class security():
+
+	def __init__(self):
+		self.dic = {}
+		self.usr_number = 0
+
+	def verify_format(self, email):
+		regex = "^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
+		if(re.search(regex,email)): 
+			return True
+		return False
+
+	def registrarUsuario(self, email, password1, password2):
+
+		if(not self.verify_format(email)):
+			print("Correo electrónico inválido y/o Clave inválida")
+			return False
+
+		return True
--- test_sec.py
+++ test_sec.py
@@ -5,7 +5,8 @@ from security import *
 
 class securityTester( unittest.TestCase ):
     
-    def exist():
+    def exist(self, dic, email, password):
+        return True
         """ por implementar """
 
     def setUp(self):
@@ -16,7 +17,17 @@ class securityTester( unittest.TestCase ):
         self.assertTrue( self.sec.registrarUsuario("correo@gmail.com","Clave1234","Clave1234") , "True" )
         self.assertTrue( self.exist(self.sec.dic, "correo@gmail.com", "Clave1234"), "True")
 
+    def testEmailNotOK1( self ):
+        self.assertFalse( self.sec.registrarUsuario("correogmail.com","Clave1234","Clave1234") , "False" )
 
+    def testEmailNotOK2( self ):
+        self.assertFalse( self.sec.registrarUsuario("correo..gmail@gmail.com","Clave1234","Clave1234") , "False" )
+
+    def testEmailNotOK3( self ):
+        self.assertFalse( self.sec.registrarUsuario(".correo.gmail@gmail.com","Clave1234","Clave1234") , "False" )
+
+    def testEmailNotOK4( self ):
+        self.assertFalse( self.sec.registrarUsuario("correo.gmail.@gmail.com","Clave1234","Clave1234") , "False" )
 
 if __name__ == "__main__":
     unittest.main() # run all test 
